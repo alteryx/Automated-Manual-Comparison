980e1684bec591bf4e684307a287bbc2a036bb11
Will Koehrsen <wjk68@case.edu>
2018-07-19 17:39:16 -0400

    Nearly done with manual feature engineering

---
+++ remaining useful life/notebooks/utils.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def plot_feature_importances(df, n = 15, threshold = None):
+    """Plots n most important features. Also plots the cumulative importance if
+    threshold is specified and prints the number of features needed to reach threshold cumulative importance.
+    Intended for use with any tree-based feature importances. 
+    
+    Args:
+        df (dataframe): Dataframe of feature importances. Columns must be "feature" and "importance".
+    
+        n (int): Number of most important features to plot. Default is 15.
+    
+        threshold (float): Threshold for cumulative importance plot. If not provided, no plot is made. Default is None.
+        
+    Returns:
+        df (dataframe): Dataframe ordered by feature importances with a normalized column (sums to 1) 
+                        and a cumulative importance column
+    
+    Note:
+    
+        * Normalization in this case means sums to 1. 
+        * Cumulative importance is calculated by summing features from most to least important
+        * A threshold of 0.9 will show the most important features needed to reach 90% of cumulative importance
+    
+    """
+    
+    # Sort features with most important at the head
+    df = df.sort_values('importance', ascending = False).reset_index(drop = True)
+    
+    # Normalize the feature importances to add up to one and calculate cumulative importance
+    df['importance_normalized'] = df['importance'] / df['importance'].sum()
+    df['cumulative_importance'] = np.cumsum(df['importance_normalized'])
+    
+    plt.rcParams['font.size'] = 12
+    
+    # Bar plot of n most important features
+    df.loc[:n, :].plot.barh(y = 'importance_normalized', 
+                            x = 'feature', color = 'blue', 
+                            edgecolor = 'k', figsize = (12, 8),
+                            legend = False)
+
+    plt.xlabel('Normalized Importance', size = 18); plt.ylabel(''); 
+    plt.title(f'Top {n} Most Important Features', size = 18)
+    plt.gca().invert_yaxis()
+    
+    
+    if threshold:
+        # Cumulative importance plot
+        plt.figure(figsize = (8, 6))
+        plt.plot(list(range(len(df))), df['cumulative_importance'], 'b-')
+        plt.xlabel('Number of Features', size = 16); plt.ylabel('Cumulative Importance', size = 16); 
+        plt.title('Cumulative Feature Importance', size = 18);
+        
+        # Number of features needed for threshold cumulative importance
+        # This is the index (will need to add 1 for the actual number)
+        importance_index = np.min(np.where(df['cumulative_importance'] > threshold))
+        
+        # Add vertical line to plot
+        plt.vlines(importance_index + 1, ymin = 0, ymax = 1.05, linestyles = '--', colors = 'red')
+        plt.show();
+        
+        print('{} features required for {:.0f}% of cumulative importance.'.format(importance_index + 1, 
+                                                                                  100 * threshold))
+    
+    return df
+
+def feature_selection(feature_matrix, missing_threshold=90, correlation_threshold=0.95):
+    """Feature selection for a dataframe."""
+    
+    feature_matrix = pd.get_dummies(feature_matrix)
+    n_features_start = feature_matrix.shape[1] - 2
+    print('Original shape: ', feature_matrix.shape)
+
+    _, idx = np.unique(feature_matrix, axis = 1, return_index = True)
+    feature_matrix = feature_matrix.iloc[:, idx]
+    n_non_unique_columns = n_features_start - feature_matrix.shape[1] - 2
+    print('{}  non-unique valued columns.'.format(n_non_unique_columns))
+
+    # Find missing and percentage
+    missing = pd.DataFrame(feature_matrix.isnull().sum())
+    missing['percent'] = 100 * (missing[0] / feature_matrix.shape[0])
+    missing.sort_values('percent', ascending = False, inplace = True)
+
+    # Missing above threshold
+    missing_cols = list(missing[missing['percent'] > missing_threshold].index)
+    n_missing_cols = len(missing_cols)
+
+    # Remove missing columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in missing_cols]]
+    print('{} missing columns with threshold: {}.'.format(n_missing_cols,
+                                                                        missing_threshold))
+    
+    # Zero variance
+    unique_counts = pd.DataFrame(feature_matrix.nunique()).sort_values(0, ascending = True)
+    zero_variance_cols = list(unique_counts[unique_counts[0] == 1].index)
+    n_zero_variance_cols = len(zero_variance_cols)
+
+    # Remove zero variance columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in zero_variance_cols]]
+    print('{} zero variance columns.'.format(n_zero_variance_cols))
+    
+    # Correlations
+    corr_matrix = feature_matrix.corr()
+
+    # Extract the upper triangle of the correlation matrix
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+
+    # Select the features with correlations above the threshold
+    # Need to use the absolute value
+    to_drop = [column for column in upper.columns if any(upper[column].abs() > correlation_threshold)]
+
+    n_collinear = len(to_drop)
+    
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in to_drop]]
+    print('{} collinear columns removed with threshold: {}.'.format(n_collinear,
+                                                                          correlation_threshold))
+    
+    total_removed = n_non_unique_columns + n_missing_cols + n_zero_variance_cols + n_collinear
+    
+    print('Total columns removed: ', total_removed)
+    print('Shape after feature selection: {}', feature_matrix.shape)
+    return feature_matrix
