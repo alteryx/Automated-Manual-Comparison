b3cb836641565899596ba2b75dd7160529cf4775
Will Koehrsen <wjk68@case.edu>
2018-07-26 18:04:25 -0400

    Working on automated feature engineering for retail

--- retail/notebooks/utils.py
+++ retail/notebooks/utils.py
@@ -107,6 +107,70 @@ def plot_feature_importances(df, n = 15, threshold = None):
     
     return df
 
+def plot_feature_importances(df, n = 15, threshold = None):
+    """Plots n most important features. Also plots the cumulative importance if
+    threshold is specified and prints the number of features needed to reach threshold cumulative importance.
+    Intended for use with any tree-based feature importances. 
+    
+    Args:
+        df (dataframe): Dataframe of feature importances. Columns must be "feature" and "importance".
+    
+        n (int): Number of most important features to plot. Default is 15.
+    
+        threshold (float): Threshold for cumulative importance plot. If not provided, no plot is made. Default is None.
+        
+    Returns:
+        df (dataframe): Dataframe ordered by feature importances with a normalized column (sums to 1) 
+                        and a cumulative importance column
+    
+    Note:
+    
+        * Normalization in this case means sums to 1. 
+        * Cumulative importance is calculated by summing features from most to least important
+        * A threshold of 0.9 will show the most important features needed to reach 90% of cumulative importance
+    
+    """
+    
+    # Sort features with most important at the head
+    df = df.sort_values('importance', ascending = False).reset_index(drop = True)
+    
+    # Normalize the feature importances to add up to one and calculate cumulative importance
+    df['importance_normalized'] = df['importance'] / df['importance'].sum()
+    df['cumulative_importance'] = np.cumsum(df['importance_normalized'])
+    
+    plt.rcParams['font.size'] = 12
+    
+    # Bar plot of n most important features
+    df.loc[:n, :].plot.barh(y = 'importance_normalized', 
+                            x = 'feature', color = 'blue', 
+                            edgecolor = 'k', figsize = (12, 8),
+                            legend = False)
+
+    plt.xlabel('Normalized Importance', size = 18); plt.ylabel(''); 
+    plt.title(f'Top {n} Most Important Features', size = 18)
+    plt.gca().invert_yaxis()
+    
+    
+    if threshold:
+        # Cumulative importance plot
+        plt.figure(figsize = (8, 6))
+        plt.plot(list(range(len(df))), df['cumulative_importance'], 'b-')
+        plt.xlabel('Number of Features', size = 16); plt.ylabel('Cumulative Importance', size = 16); 
+        plt.title('Cumulative Feature Importance', size = 18);
+        
+        # Number of features needed for threshold cumulative importance
+        # This is the index (will need to add 1 for the actual number)
+        importance_index = np.min(np.where(df['cumulative_importance'] > threshold))
+        
+        # Add vertical line to plot
+        plt.vlines(importance_index + 1, ymin = 0, ymax = 1.05, linestyles = '--', colors = 'red')
+        plt.show();
+        
+        print('{} features required for {:.0f}% of cumulative importance.'.format(importance_index + 1, 
+                                                                                  100 * threshold))
+    
+    return df
+
 def feature_selection(feature_matrix, missing_threshold=90, correlation_threshold=0.95):
     """Feature selection for a dataframe."""
     
